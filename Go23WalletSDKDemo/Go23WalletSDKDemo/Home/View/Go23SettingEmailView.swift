6735bf38852d1f7981cfc81d07d6c0455c8bb724
Taran <NA>
2023-01-17 10:59:46 +0800

    add third login button

--- Go23WalletSDKDemo/Go23WalletSDKDemo/Home/View/Go23SettingEmailView.swift
+++ Go23WalletSDKDemo/Go23WalletSDKDemo/Home/View/Go23SettingEmailView.swift
@@ -15,7 +15,6 @@ class Go23SettingEmailView: UIView {
         super.init(frame: frame)
         initSubviews()
         changeSendBtnStatus(status: false)
-         
     }
     
     required init?(coder: NSCoder) {
@@ -27,24 +26,16 @@ class Go23SettingEmailView: UIView {
 
         backgroundColor = .white
         addSubview(titleLabel)
-//        addSubview(closeBtn)
         addSubview(emailLabel)
         addSubview(emailTxtFiled)
-//        addSubview(codeLabel)
-//        addSubview(codeTxtFiled)
-//        addSubview(sendBtn)
         addSubview(confirmBtn)
+        addSubview(loginThirdBtn)
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(14)
             make.centerX.equalToSuperview()
             make.height.equalTo(32)
         }
-//        closeBtn.snp.makeConstraints { make in
-//            make.trailing.equalTo(0)
-//            make.width.height.equalTo(44)
-//            make.centerY.equalTo(titleLabel.snp.centerY)
-//        }
         
         emailLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(30)
@@ -57,48 +48,25 @@ class Go23SettingEmailView: UIView {
             make.trailing.equalTo(-20)
             make.height.equalTo(46)
         }
-
-//        codeLabel.snp.makeConstraints { make in
-//            make.top.equalTo(emailTxtFiled.snp.bottom).offset(12)
-//            make.leading.equalTo(20)
-//            make.height.equalTo(22)
-//        }
-//        codeTxtFiled.snp.makeConstraints { make in
-//            make.top.equalTo(codeLabel.snp.bottom).offset(8)
-//            make.leading.equalTo(20)
-//            make.trailing.equalTo(-20)
-//            make.height.equalTo(46)
-//        }
-//        sendBtn.snp.makeConstraints { make in
-//            make.height.equalTo(44)
-//            make.width.equalTo(60)
-//            make.trailing.equalTo(-20)
-//            make.centerY.equalTo(codeTxtFiled.snp.centerY)
-//        }
+        
         confirmBtn.snp.makeConstraints { make in
-//            if #available(iOS 11.0, *) {
-//                 make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
-//             } else {
-//                 make.bottom.equalTo(0)
-//            }
             make.top.equalTo(emailTxtFiled.snp.bottom).offset(50)
             make.leading.equalTo(20)
             make.trailing.equalTo(-20)
             make.height.equalTo(46)
         }
         
-
+        loginThirdBtn.snp.makeConstraints { make in
+            make.top.equalTo(confirmBtn.snp.bottom).offset(20)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(46)
+        }
     }
-    
     
     @objc private func confirmBtnClick() {
         
         if let emailT = emailTxtFiled.text, !validateEmail(email: emailT) {
-//            let hud = MBProgressHUD.showAdded(to: self, animated: true)
-//            hud.mode = .text
-//            hud.label.text = "Email input error!"
-//            hud.label.font = UIFont(name: NotoSans, size: 16)
-//            hud.hide(animated: true, afterDelay: 1)
             let totast = Go23Toast.init(frame: .zero)
             totast.show("Email input error!", after: 1)
             return
@@ -109,16 +77,15 @@ class Go23SettingEmailView: UIView {
             self.confirmBlock?()
             return
         }
-//        let hud = MBProgressHUD.showAdded(to: self, animated: true)
-//        hud.mode = .text
-//        hud.label.text = "Please enter email!"
-//        hud.label.font = UIFont(name: NotoSans, size: 16)
-//        hud.hide(animated: true, afterDelay: 1)
         
         let totast = Go23Toast.init(frame: .zero)
         totast.show("Please enter email!", after: 1)
     }
     
+    @objc private func thirdLoginClick() {
+        
+        
+    }
     
     func validateEmail(email: String) -> Bool {
         if email.count == 0 {
@@ -128,7 +95,6 @@ class Go23SettingEmailView: UIView {
         let emailTest:NSPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailTest.evaluate(with: email)
     }
-    
     
     @objc private func closeBtnClick() {
         self.closeBlock?()
@@ -156,9 +122,6 @@ class Go23SettingEmailView: UIView {
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-//        label.text = "Set Pincode"
-//        label.font = UIFont.init(name: BarlowCondensed, size: 24)
-//        label.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
         label.attributedText = String.getAttributeString(font: UIFont(name: BarlowCondensed, size: 20), wordspace: 0.5, color: UIColor.rdt_HexOfColor(hexString: "#262626"),alignment: .center, title: "Login")
 
         return label
@@ -167,7 +130,6 @@ class Go23SettingEmailView: UIView {
     private lazy var emailLabel: UILabel = {
         let label = UILabel()
         label.text = "Email"
-//        label.font = UIFont(name: NotoSans, size: 14)
         label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
         return label
@@ -175,26 +137,14 @@ class Go23SettingEmailView: UIView {
     
     private lazy var emailTxtFiled: UITextField = {
         let textfield = UITextField()
-//        let textplace = "Enter PIN Code"
-//        let placeholder = NSMutableAttributedString()
-//        placeholder.add(text: textplace) { (attributes) in
-//            attributes.customFont(12.0, NotoSans)
-//        }
-//        textfield.attributedPlaceholder = placeholder
         textfield.autocapitalizationType = .none
         textfield.autocorrectionType = .no
-//        textfield.font = UIFont(name: NotoSans, size: 14)
         textfield.font = UIFont.systemFont(ofSize: 14)
         textfield.tintColor = UIColor.rdt_HexOfColor(hexString: "#262626")
-//        textfield.becomeFirstResponder()
         textfield.leftViewMode = .always
         textfield.leftView = UIView.init(frame: CGRectMake(0, 0, 15, 0))
         textfield.clearButtonMode = .always
         textfield.layer.cornerRadius = 8
-//        textfield.isSecureTextEntry = true
-//        textfield.layer.masksToBounds = true
-//        textfield.layer.borderWidth = 1
-//        textfield.layer.borderColor = UIColor.rdt_HexOfColor(hexString: "#D9D9D9").cgColor
         textfield.backgroundColor = UIColor.rdt_HexOfColor(hexString: "#F5F5F5")
         textfield.addTarget(self, action: #selector(textDidChange(_ :)), for: .editingChanged)
         return textfield
@@ -203,7 +153,6 @@ class Go23SettingEmailView: UIView {
     private lazy var codeLabel: UILabel = {
         let label = UILabel()
         label.text = "Verification code"
-//        label.font = UIFont(name: NotoSans, size: 14)
         label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
         return label
@@ -211,24 +160,12 @@ class Go23SettingEmailView: UIView {
     
     private lazy var codeTxtFiled: UITextField = {
         let textfield = UITextField()
-//        let textplace = "Enter PIN Code"
-//        let placeholder = NSMutableAttributedString()
-//        placeholder.add(text: textplace) { (attributes) in
-//            attributes.customFont(12.0, NotoSans)
-//        }
-//        textfield.attributedPlaceholder = placeholder
-//        textfield.font = UIFont(name: NotoSans, size: 14)
         textfield.font = UIFont.systemFont(ofSize: 14)
         textfield.tintColor = UIColor.rdt_HexOfColor(hexString: "#262626")
-//        textfield.becomeFirstResponder()
         textfield.leftViewMode = .always
         textfield.leftView = UIView.init(frame: CGRectMake(0, 0, 15, 0))
-//        textfield.clearButtonMode = .always
         textfield.layer.cornerRadius = 8
         textfield.isSecureTextEntry = true
-//        textfield.layer.masksToBounds = true
-//        textfield.layer.borderWidth = 1
-//        textfield.layer.borderColor = UIColor.rdt_HexOfColor(hexString: "#D9D9D9").cgColor
         textfield.backgroundColor = UIColor.rdt_HexOfColor(hexString: "#F5F5F5")
         return textfield
     }()
@@ -237,11 +174,17 @@ class Go23SettingEmailView: UIView {
         let btn = UIButton(type: .custom)
         btn.layer.cornerRadius = 8
         btn.backgroundColor = UIColor.rdt_HexOfColor(hexString: "#00D6E1")
-//        btn.setTitle("Confirm", for: .normal)
-//        btn.setTitleColor(.white, for: .normal)
-//        btn.titleLabel?.font = UIFont(name: BarlowCondensed, size: 24)
         btn.addTarget(self, action: #selector(confirmBtnClick), for: .touchUpInside)
         btn.setAttributedTitle(String.getAttributeString(font: UIFont(name: BarlowCondensed, size: 24), wordspace: 0.5, color: UIColor.white,alignment: .center, title: "Confirm"), for: .normal)
+        return btn
+    }()
+    
+    private lazy var loginThirdBtn: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.layer.cornerRadius = 8
+        btn.backgroundColor = UIColor.rdt_HexOfColor(hexString: "#FFFFFF")
+        btn.addTarget(self, action: #selector(confirmBtnClick), for: .touchUpInside)
+        btn.setAttributedTitle(String.getAttributeString(font: UIFont(name: BarlowCondensed, size: 20), wordspace: 0.5, color: UIColor.rdt_HexOfColor(hexString: "#00D6E1"), alignment: .center, title: "Login with coins"), for: .normal)
         return btn
     }()
     
@@ -249,7 +192,6 @@ class Go23SettingEmailView: UIView {
         let btn = UIButton(type: .custom)
         btn.setTitle("Send", for: .normal)
         btn.setTitleColor(UIColor.rdt_HexOfColor(hexString: "#00D6E1"), for: .normal)
-//        btn.titleLabel?.font = UIFont(name: NotoSans, size: 14)
         btn.titleLabel?.font = UIFont.systemFont(ofSize: 14)
         btn.addTarget(self, action: #selector(sendClick), for: .touchUpInside)
         return btn
@@ -260,7 +202,6 @@ class Go23SettingEmailView: UIView {
 
 extension Go23SettingEmailView {
     @objc func textDidChange(_ textField:UITextField) {
-        print("event:\(textField.text)")
         if let txt = textField.text {
             if txt.count > 0 {
                 changeSendBtnStatus(status: true)
