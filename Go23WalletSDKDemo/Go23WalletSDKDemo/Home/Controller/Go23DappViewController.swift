47dc7e716d4d4774b2aa981b431271c1c94cdbc4
ming.lu <ming.lu@coins.ph>
2023-02-01 11:39:53 +0800

    modify UI

--- Go23WalletSDKDemo/Go23WalletSDKDemo/Home/Controller/Go23DappViewController.swift
+++ Go23WalletSDKDemo/Go23WalletSDKDemo/Home/Controller/Go23DappViewController.swift
@@ -9,21 +9,66 @@ import UIKit
 
 class Go23DappViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = false
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNav()
+        initSubviews()
+    }
+    
+    private func setNav() {
+        
+        if self.navgationBar == nil {
+            addBarView()
+            navgationBar?.title = "Dapp"
+            navgationBar?.attributes = [NSAttributedString.Key.font: UIFont(name: BarlowCondensed, size: 20), NSAttributedString.Key.kern: 0.5] as [NSAttributedString.Key : Any]
+        }
+    }
+    
+    
+    private func initSubviews() {
+        view.backgroundColor = .white
+        view.addSubview(scrollView)
+        scrollView.addSubview(scrollContentView)
+        
+        scrollView.snp.makeConstraints { make in
+
+            make.top.equalTo(navgationBar!.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        scrollContentView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.width.equalTo(ScreenWidth)
+            make.height.equalTo(ScreenHeight)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    
+    private lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.contentSize = CGSize(width: 0, height: ScreenHeight)
+        sv.showsVerticalScrollIndicator = false
+        sv.showsHorizontalScrollIndicator = false
+        sv.contentInsetAdjustmentBehavior = .never
+        return sv
+    }()
+    
+    private lazy var scrollContentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+
 
 }
--- Go23WalletSDKDemo/Go23WalletSDKDemo/Home/Controller/Go23MeViewController.swift
+++ Go23WalletSDKDemo/Go23WalletSDKDemo/Home/Controller/Go23MeViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class Go23MeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = false
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNav()
+        initSubviews()
+    }
+    
+    private func setNav() {
+        
+        if self.navgationBar == nil {
+            addBarView()
+            navgationBar?.title = "Me"
+            navgationBar?.attributes = [NSAttributedString.Key.font: UIFont(name: BarlowCondensed, size: 20), NSAttributedString.Key.kern: 0.5] as [NSAttributedString.Key : Any]
+        }
+    }
+    
+    
+    private func initSubviews() {
+        view.backgroundColor = .white
+    }
 
 }
