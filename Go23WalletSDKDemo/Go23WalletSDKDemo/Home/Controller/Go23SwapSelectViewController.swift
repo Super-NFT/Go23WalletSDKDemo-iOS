5a5f4543e77bc339f84c1fd4fc4c95987ce90b81
ming.lu <ming.lu@coins.ph>
2023-02-21 19:37:22 +0800

    swap select token view

---
+++ Go23WalletSDKDemo/Go23WalletSDKDemo/Home/Controller/Go23SwapSelectViewController.swift
@@ -0,0 +1,333 @@
+//
+//  Go23SwapSelectViewController.swift
+//  Go23WalletSDKDemo
+//
+//  Created by luming on 2023/2/21.
+//
+
+import UIKit
+import Kingfisher
+import Go23SDK
+
+
+class Go23SwapSelectViewController: UIViewController {
+
+    
+    var tokenList: [Go23ChainTokenModel]?
+    var tokenIndex = 1
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 13.0, *) {
+            self.overrideUserInterfaceStyle = .light
+        }
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(navgationBar!.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        getUserTokens()
+        
+        tableView.es.addPullToRefresh {[weak self] in
+            self?.tokenIndex = 1
+            self?.getUserTokens(isLoading: false)
+        }
+        tableView.es.addInfiniteScrolling { [weak self] in
+            self?.tokenIndex += 1
+            self?.getUserTokens(isLoading: false)
+        }
+
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .none
+        tableView.estimatedRowHeight = 0
+        tableView.estimatedSectionHeaderHeight = 0
+        tableView.estimatedSectionFooterHeight = 0
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(SwapTokenViewCell.self, forCellReuseIdentifier: SwapTokenViewCell.reuseIdentifier())
+
+        return tableView
+    }()
+    
+
+}
+
+
+extension Go23SwapSelectViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tokenList?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AddTokenViewCell.reuseIdentifier(), for: indexPath) as? AddTokenViewCell, let count = self.tokenList?.count,indexPath.row < count
+        else {
+            return UITableViewCell()
+        }
+        
+        if let model = self.tokenList?[indexPath.row] {
+            cell.filled(model: model, isHidden: indexPath.row == 0)
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Go23ChooseAlertViewCell.cellHeight
+        
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        if let count = self.tokenList?.count, indexPath.row < count {
+            
+        }
+        
+        if let count = self.tokenList?.count, indexPath.row < count,let model = self.tokenList?[indexPath.row]{
+//
+            if model.isPlatform {
+                return
+            }
+            
+            if model.isSelected {
+                hideToken(model: model)
+                return
+            }
+            addToken(model: model)
+            
+        }
+    }
+    
+}
+    
+    
+    
+class SwapTokenViewCell: UITableViewCell {
+        static var cellHeight: CGFloat {
+            return 60.0
+        }
+        
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+
+            // Configure the view for the selected state
+        }
+        
+        
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            selectionStyle = .none
+            initSubviews()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        private func initSubviews() {
+            contentView.backgroundColor = .white
+            contentView.addSubview(iconImgv)
+            contentView.addSubview(titleLabel)
+            contentView.addSubview(contractLabel)
+            contentView.addSubview(numLabel)
+            contentView.addSubview(moneyLabel)
+            
+            iconImgv.snp.makeConstraints { make in
+                make.centerY.equalToSuperview()
+                make.leading.equalTo(20)
+                make.width.height.equalTo(28)
+            }
+            
+            titleLabel.snp.makeConstraints { make in
+                make.top.equalTo(9)
+                make.left.equalTo(iconImgv.snp.right).offset(12)
+                make.height.equalTo(24)
+            }
+            
+            contractLabel.snp.makeConstraints { make in
+                make.top.equalTo(titleLabel.snp.bottom).offset(0)
+                make.left.equalTo(iconImgv.snp.right).offset(12)
+                make.height.equalTo(18)
+            }
+            
+            numLabel.snp.makeConstraints { make in
+                make.trailing.equalTo(-20)
+                make.top.equalTo(9)
+                make.height.equalTo(24)
+            }
+            
+            moneyLabel.snp.makeConstraints { make in
+                make.trailing.equalTo(-20)
+                make.top.equalTo(numLabel.snp.bottom).offset(0)
+                make.height.equalTo(18)
+                
+            }
+        }
+        
+        func filled(model: Go23ChainTokenModel, isHidden: Bool) {
+            iconImgv.kf.setImage(with: URL(string: model.imageUrl))
+            titleLabel.text = model.symbol
+            numLabel.text = "0.00"
+            moneyLabel.text = "$0.00"
+            if Float(model.balance) ?? 0.0 > 0 {
+                numLabel.text = model.balance
+            }
+            if Float(model.balanceU) ?? 0.0 > 0 {
+                moneyLabel.text = "$\(model.balanceU)"
+            }
+            
+            if isHidden {
+                titleLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#BFBFBF")
+                numLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#BFBFBF")
+                moneyLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#BFBFBF")
+                contractLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#BFBFBF")
+                contractLabel.text = model.chainName
+            } else {
+                titleLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
+                numLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
+                moneyLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#8C8C8C")
+                contractLabel.textColor = UIColor.rdt_HexOfColor(hexString: "#8C8C8C")
+                contractLabel.text = String.getSubSecretString(8,string: model.contractAddress)
+            }
+        }
+        
+        private lazy var iconImgv: UIImageView = {
+            let imgv = UIImageView()
+            
+            return imgv
+        }()
+        
+        private lazy var titleLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 16)
+            label.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
+            return label
+        }()
+        
+        private lazy var numLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 16)
+            label.textAlignment = .right
+            label.text = "0.00"
+            label.textColor = UIColor.rdt_HexOfColor(hexString: "#262626")
+            return label
+        }()
+        
+        private lazy var contractLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textColor = UIColor.rdt_HexOfColor(hexString: "#8C8C8C")
+            return label
+        }()
+        
+        private lazy var moneyLabel: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textAlignment = .right
+            label.text = "$0.00"
+            label.textColor = UIColor.rdt_HexOfColor(hexString: "#8C8C8C")
+            return label
+        }()
+        
+    }
+
+
+extension Go23SwapSelectViewController {
+    @objc private func getUserTokens(isLoading: Bool = true) {
+        guard let shared = Go23WalletSDK.shared
+        else {
+            return
+        }
+        
+       guard let walletObj = Go23WalletMangager.shared.walletModel else {
+           return
+           
+       }
+        if isLoading {
+            Go23Loading.loading()
+        }
+       
+       shared.getChainTokenList(with: walletObj.chainId, pageSize: 20, pageNumber: self.tokenIndex) { [weak self] model in
+           self?.tableView.es.stopPullToRefresh()
+           self?.tableView.es.stopLoadingMore()
+           if isLoading {
+               Go23Loading.clear()
+           }
+           if self?.tokenIndex ?? 1 > 1 {
+               if let _ = self?.tokenList, let _ = model?.listModel {
+                   self?.tokenList! += model!.listModel
+               }
+           } else {
+               self?.tokenList?.removeAll()
+               self?.tokenList = model?.listModel
+           }
+           
+           self?.tableView.reloadData()
+       }
+        
+    }
+    
+    func hideToken(model: Go23ChainTokenModel) {
+        guard let shared = Go23WalletSDK.shared
+        else {
+            return
+        }
+        
+        Go23Loading.loading()
+        guard let walletObj = Go23WalletMangager.shared.walletModel else {
+        return
+        }
+        shared.hideToken(with: model.chainId, walletAddress: Go23WalletMangager.shared.address, contractAddress: model.contractAddress) { [weak self](data) in
+            print(data)
+            Go23Loading.clear()
+            self?.getUserTokens()
+        }
+        
+
+    }
+    
+    func addToken(model: Go23ChainTokenModel) {
+        guard let shared = Go23WalletSDK.shared
+        else {
+            return
+        }
+        Go23Loading.loading()
+        shared.addToken(with: model.chainId, walletAddress: Go23WalletMangager.shared.address, contractAddress: model.contractAddress) { [weak self](data) in
+            Go23Loading.clear()
+            self?.getUserTokens()
+        }
+        
+    }
+}
+
+//MARK: - pragma mark =========== JXSegmentedListContainerViewListDelegate ===========
+extension Go23SwapSelectViewController: JXSegmentedListContainerViewListDelegate {
+   func listView() -> UIView {
+       return view
+   }
+}
--- Go23WalletSDKDemo/Go23WalletSDKDemo/Home/View/Go23SwipSelectView.swift
+++ Go23WalletSDKDemo/Go23WalletSDKDemo/Home/View/Go23SwipSelectView.swift
@@ -6,15 +6,158 @@
 //
 
 import UIKit
+import Go23SDK
+import Kingfisher
 
 class Go23SwipSelectView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var tokenList: [Go23WalletTokenModel]?
+    private var tokenIndex = 1
+    
+    var clickBlock: ((_ model: Go23WalletTokenModel)->())?
+    
+    var addBtnBlock:(()->())?
+    
+    var closeBlock:(()->())?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        if #available(iOS 13.0, *) {
+            self.overrideUserInterfaceStyle = .light
+        }
+        initSubviews()
+         
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initSubviews() {
+        roundCorners([.topLeft,.topRight], radius: 12)
+        
+        backgroundColor = .white
+        addSubview(titleLabel)
+        addSubview(closeBtn)
+        
+        
 
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(14)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(32)
+        }
+        closeBtn.snp.makeConstraints { make in
+            make.trailing.equalTo(-6)
+            make.width.height.equalTo(44)
+            make.centerY.equalTo(titleLabel.snp.centerY)
+        }
+        
+        
+        //        addSubview(addBtn)
+//        addBtn.snp.makeConstraints { make in
+//            if #available(iOS 11.0, *) {
+//                make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(0)
+//             } else {
+//                 make.bottom.equalTo(0)
+//            }
+//            make.left.equalTo(20)
+//            make.right.equalTo(-20)
+//            make.height.equalTo(52)
+//        }
+        
+    }
+   
+    @objc private func closeBtnClick() {
+        self.closeBlock?()
+        
+    }
+    
+    @objc private func addBtnClick() {
+        addBtnBlock?()
+    }
+        
+    private lazy var closeBtn: UIButton = {
+            let btn = UIButton(type: .custom)
+            let imgv = UIImageView()
+            imgv.image = UIImage.init(named: "close")
+            btn.addSubview(imgv)
+            imgv.snp.makeConstraints { make in
+                make.center.equalToSuperview()
+                make.height.equalTo(13)
+                make.width.equalTo(13)
+            }
+            btn.addTarget(self, action: #selector(closeBtnClick), for: .touchUpInside)
+            return btn
+        }()
+        
+    private lazy var titleLabel: UILabel = {
+            let label = UILabel()
+        label.attributedText = String.getAttributeString(font: UIFont(name: BarlowCondensed, size: 20), wordspace: 0.5, color: UIColor.rdt_HexOfColor(hexString: "#262626"),alignment: .center, title: "Select a token to send")
+            return label
+        }()
+    
+    private lazy var addBtn: UIButton = {
+        let btn = UIButton()
+        btn.layer.cornerRadius = 8
+        btn.backgroundColor = UIColor.rdt_HexOfColor(hexString: "#00D6E1")
+        btn.setAttributedTitle(String.getAttributeString(font: UIFont(name: BarlowCondensed, size: 24), wordspace: 0.5, color: UIColor.white, alignment: .center, title: "Add a token"), for: .normal)
+        btn.addTarget(self, action: #selector(addBtnClick), for: .touchUpInside)
+        return btn
+    }()
+    
+    let dataSource: JXSegmentedTitleDataSource = JXSegmentedTitleDataSource()
+    
+    private lazy var listContainerView: JXSegmentedListContainerView! = {
+        return JXSegmentedListContainerView(dataSource: self)
+    }()
+    
+    private lazy var segmentedView: JXSegmentedView = {
+        
+        let segmentedView = JXSegmentedView()
+        segmentedView.backgroundColor = UIColor.rdt_HexOfColor(hexString: "#F9F9F9")
+
+        segmentedView.delegate = self
+        segmentedView.isContentScrollViewClickTransitionAnimationEnabled = false
+        
+        let indicator = JXSegmentedIndicatorLineView()
+        indicator.indicatorWidth = 50
+        indicator.indicatorHeight = 3
+        indicator.verticalOffset = 1
+        indicator.indicatorColor = UIColor.rdt_HexOfColor(hexString: "#00D6E1")
+        segmentedView.indicators = [indicator]
+        
+        return segmentedView
+    }()
+   
+    }
+
+// MARK: - pragma mark =========== JXSegmentedViewDelegate ===========
+
+extension Go23SwipSelectView: JXSegmentedViewDelegate {
+    func segmentedView(_ segmentedView: JXSegmentedView, didSelectedItemAt index: Int) {
+            segmentedView.dataSource = dataSource
+            segmentedView.reloadItem(at: index)
+    }
 }
+
+// MARK: - pragma mark =========== JXSegmentedListContainerViewDataSource ===========
+extension Go23SwipSelectView: JXSegmentedListContainerViewDataSource {
+    func numberOfLists(in listContainerView: JXSegmentedListContainerView) -> Int {
+        if let titleDataSource = segmentedView.dataSource as? JXSegmentedBaseDataSource {
+            return titleDataSource.dataSource.count
+        }
+        return 0
+    }
+
+    func listContainerView(_ listContainerView: JXSegmentedListContainerView, initListAt index: Int) -> JXSegmentedListContainerViewListDelegate {
+  
+        let vc = Go23SwapSelectViewController()
+        return vc
+
+
+    }
+    
+}
+    
